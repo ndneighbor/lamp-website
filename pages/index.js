2d15ae9049d1c16012a26035dfe015b5f6fa859f
ndneighbor <asara019@fiu.edu>
2021-06-02 20:59:55 -0500

    dark mode

--- pages/index.js
+++ pages/index.js
@@ -5,83 +5,61 @@ import { jsx, ThemeProvider } from "@emotion/react";
 import styled from "@emotion/styled";
 import { AnimatePresence, motion } from "framer-motion";
 
-// 3. Style items
 // 4. Light mode when you click lamp, default dark, when lit you hear music
+// 5. Add the audio stuff into
 
 // PLaceholder theme provider for the dark mode
-const theme = {
+const themeLight = {
   colors: {
-    primary: "hotpink",
+    primary: "black",
+    secondary: "white",
+  },
+};
+
+const themeDark = {
+  colors: {
+    primary: "white",
+    secondary: "black",
   },
 };
 
 const SomeText = styled.div`
-  color: ${(props) => props.theme.colors.primary};
+  display: flex;
+  justify-content: center;
+  font-size: 24px;
 `;
 
-export const letterContainerVariants = {
-  before: { transition: { staggerChildren: 0.015 } },
-  after: { transition: { staggerChildren: 0.03 } },
-};
-
-// Variants for animating each letter
-export const letterVariants = {
-  before: {
-    y: 20,
-    transition: {
-      type: "spring",
-      damping: 12,
-      stiffness: 200,
-    },
-  },
-  after: {
-    y: 0,
-    transition: {
-      type: "spring",
-      damping: 12,
-      stiffness: 200,
-    },
-  },
-};
-
 const Container = styled.div`
+  color: ${(props) => props.theme.colors.primary};
+  background: ${(props) => props.theme.colors.secondary};
   display: flex;
   justify-content: center;
   align-self: center;
+  font-family: "Papyrus";
 `;
 
 const LampText = styled.div`
   justify-content: center;
   margin: 24px;
   display: flex;
-  font-family: "Papyrus";
   font-size: 64px;
   margin: 24px;
 `;
 
 export default function Home() {
-  return (
-    <Container>
-      <div>
-        <motion.img
-          animate={{
-            translateY: [-10, 10, -10, 10, -10],
-          }}
-          transition={{
-            duration: 3,
-            ease: "easeInOut",
-            loop: Infinity,
-          }}
-          src="/TransparentBG.png"
-          alt="Logo of the Lamp Project"
-          width={500}
-          height={500}
-        ></motion.img>
+  const [darkMode, setDarkMode] = useState(false);
 
-        {/* No copy at all. */}
-        {/* Need to add the wave effect into an animated */}
-        <LampText>
-          <motion.div
+  function handleImageClick() {
+    setDarkMode(!darkMode);
+    console.log(darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : themeLight}>
+      <Container>
+        <div>
+          <motion.img
+            onClick={handleImageClick}
             animate={{
               translateY: [-10, 10, -10, 10, -10],
             }}
@@ -90,52 +68,67 @@ export default function Home() {
               ease: "easeInOut",
               loop: Infinity,
             }}
-          >
-            L
-          </motion.div>
-          <motion.div
-            animate={{
-              translateY: [-10, 10, -10, 10, -10],
-            }}
-            transition={{
-              duration: 2.9,
-              ease: "easeInOut",
-              loop: Infinity,
-            }}
-          >
-            A
-          </motion.div>
-          <motion.div
-            animate={{
-              translateY: [-10, 10, -10, 10, -10],
-            }}
-            transition={{
-              duration: 2.8,
-              ease: "easeInOut",
-              loop: Infinity,
-            }}
-          >
-            M
-          </motion.div>
-          <motion.div
-            animate={{
-              translateY: [-10, 10, -10, 10, -10],
-            }}
-            transition={{
-              duration: 2.7,
-              ease: "easeInOut",
-              loop: Infinity,
-            }}
-          >
-            P
-          </motion.div>
-        </LampText>
+            src="/TransparentBG.png"
+            alt="Logo of the Lamp Project"
+            width={500}
+            height={500}
+          ></motion.img>
 
-        <h2>Whitepaper</h2>
-        <ThemeProvider theme={theme}>
-          <SomeText>some text</SomeText>
-        </ThemeProvider>
-      </div>
-    </Container>
+          {/* No copy at all. */}
+          {/* Need to add the wave effect into an animated */}
+          <LampText>
+            <motion.div
+              animate={{
+                translateY: [-10, 10, -10, 10, -10],
+              }}
+              transition={{
+                duration: 3,
+                ease: "easeInOut",
+                loop: Infinity,
+              }}
+            >
+              L
+            </motion.div>
+            <motion.div
+              animate={{
+                translateY: [-10, 10, -10, 10, -10],
+              }}
+              transition={{
+                duration: 2.9,
+                ease: "easeInOut",
+                loop: Infinity,
+              }}
+            >
+              A
+            </motion.div>
+            <motion.div
+              animate={{
+                translateY: [-10, 10, -10, 10, -10],
+              }}
+              transition={{
+                duration: 2.8,
+                ease: "easeInOut",
+                loop: Infinity,
+              }}
+            >
+              M
+            </motion.div>
+            <motion.div
+              animate={{
+                translateY: [-10, 10, -10, 10, -10],
+              }}
+              transition={{
+                duration: 2.7,
+                ease: "easeInOut",
+                loop: Infinity,
+              }}
+            >
+              P
+            </motion.div>
+          </LampText>
+          <SomeText>Whitepaper</SomeText>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
